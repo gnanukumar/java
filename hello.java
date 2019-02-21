71b556b60b8292e842fb1992b9e39e4cee60a0ae
gnanukumar <47548685+gnanukumar@users.noreply.github.com>
2019-02-21 14:45:01 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Hello{
+public static void main(String[] qwe){
+Scanner scan=new Scanner(System.in);
+int N=scan.nextInt();
+int x=1;
+while(x<=N){
+System.out.println("hello");
+x++;
+}
+}
+}
